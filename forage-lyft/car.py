069220f531114fb53c8781977b2970335a576d56
Yanqi Zhu <abbyzhu@192-168-1-139.tpgi.com.au>
2023-05-21 21:16:31 +1000

    initial-task2

---
+++ forage-lyft/car.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+
+class Car(ABC):
+    def __init__(self, last_service_date, engine, battery):
+        self.last_service_date = last_service_date
+        self.engine = engine
+        self.battery = battery
+
+    @abstractmethod
+    def needs_service(self):
+        return self.engine.needs_service() or self.battery.needs_service()
+
